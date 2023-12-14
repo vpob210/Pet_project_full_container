e2fed655648fde98e7ca423114e0a70611498e9a
vpob210 <146315049+vpob210@users.noreply.github.com>
2023-12-14 12:29:23 +0500

    Update simple_app.sql

--- mysql/simple_app.sql
+++ mysql/simple_app.sql
@@ -15,9 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- DB
-CREATE DATABASE todo_app;
-USE todo_app;
 
 --
 -- Table structure for table `key_values`
